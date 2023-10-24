b3a9b653bd01a26b5ad671c839742f22594c5645
Fergus Hewson <Fergus_Hewson@bnz.co.nz>
2023-10-25 07:26:09 +1300

    Fixing iOS tests.
    Updating build script to publish debug artifact.

--- shared/src/iosMain/kotlin/com/timerx/repository/IOSTimerRepository.kt
+++ shared/src/iosMain/kotlin/com/timerx/repository/IOSTimerRepository.kt
@@ -1,10 +1,14 @@
 package com.timerx.repository
 
 import com.timerx.domain.Timer
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
 
 class IOSTimerRepository : TimerRepository {
-    override val timers: List<Timer>
-        get() = wow()
+
+    override fun timers(): PersistentList<Timer> {
+        return persistentListOf()
+    }
 }
 
 actual fun getTimerRepository(): TimerRepository = IOSTimerRepository()
