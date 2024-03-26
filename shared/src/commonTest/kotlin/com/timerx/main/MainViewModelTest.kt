884a72bc3bfe34e0c14d5215e774f82c1b91d18f
Fergus Hewson <Fergus_Hewson@bnz.co.nz>
2024-03-26 13:33:43 +1300

    Adding MainViewModelTest

---
+++ shared/src/commonTest/kotlin/com/timerx/main/MainViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.timerx.main
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class MainViewModelTest {
+
+    @Test
+    fun feelGood() {
+        assertEquals(expected = true, actual = true)
+    }
+
+}
