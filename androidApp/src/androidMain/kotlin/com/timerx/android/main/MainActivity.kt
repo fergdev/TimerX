74e62ba552f80ebda2c92d595688e58568087fb3
Fergus Hewson <Fergus_Hewson@bnz.co.nz>
2024-03-28 09:18:17 +1300

    Adding edge to edge for android app.

--- androidApp/src/androidMain/kotlin/com/timerx/android/main/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/timerx/android/main/MainActivity.kt
@@ -3,11 +3,13 @@ package com.timerx.android.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.timerx.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             App()
         }
