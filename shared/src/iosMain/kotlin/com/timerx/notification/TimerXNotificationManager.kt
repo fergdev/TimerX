fbbf4e015af8c1edf7dbd9a761235b0dc65dbf11
fergdev <fergus.a.hewson@outlook.com>
2024-06-06 20:46:12 +1200

    Fix for detekt.

--- shared/src/iosMain/kotlin/com/timerx/notification/TimerXNotificationManager.kt
+++ shared/src/iosMain/kotlin/com/timerx/notification/TimerXNotificationManager.kt
@@ -2,12 +2,15 @@ package com.timerx.notification
 
 actual class TimerXNotificationManager actual constructor() {
     actual fun startService() {
+        // Noop
     }
 
     actual fun stopService() {
+        // Noop
     }
 
     actual fun updateNotification(info: String) {
+        // Noop
     }
 
 }
