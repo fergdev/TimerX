df1e3e77cab8f2091b8dbbe809aa255db7cc1077
fergdev <fergus.a.hewson@outlook.com>
2024-06-06 21:04:29 +1200

    Small cleanup.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
