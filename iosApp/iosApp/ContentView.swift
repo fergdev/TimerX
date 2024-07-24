fee95e6d4a598937a7ae06d7c6cb071e5c8f086d
fergdev <fergus.a.hewson@outlook.com>
2024-07-24 13:57:30 +1200

    Bumping room version.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -26,12 +26,6 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
-}
-
 struct BannerView: UIViewRepresentable {
     var adUnitID: String
 
