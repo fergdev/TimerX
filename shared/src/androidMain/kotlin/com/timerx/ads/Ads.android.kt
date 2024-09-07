71f5d6427a533c894567fa8de91723a3aedb4e4f
fergdev <fergus.a.hewson@outlook.com>
2024-09-08 09:53:27 +1200

    Adding more ad logging.

--- shared/src/androidMain/kotlin/com/timerx/ads/Ads.android.kt
+++ shared/src/androidMain/kotlin/com/timerx/ads/Ads.android.kt
@@ -55,7 +55,12 @@ actual fun GoogleAd() {
             AdView(androidViewContext).apply {
                 setAdSize(adSize)
                 adUnitId = "ca-app-pub-2499949091653906/6117628581"
-                loadAd(AdRequest.Builder().build())
+                loadAd(AdRequest.Builder()
+                    .addKeyword("fitness")
+                    .addKeyword("gym")
+                    .addKeyword("health")
+                    .addKeyword("workout")
+                    .build())
                 adListener =
                     object : AdListener() {
                         override fun onAdLoaded() {
@@ -63,7 +68,13 @@ actual fun GoogleAd() {
                         }
 
                         override fun onAdFailedToLoad(error: LoadAdError) {
-                            Log.e(TAG, "Banner ad failed to load.")
+                            Log.e(
+                                TAG,
+                                "Banner ad failed to load. " +
+                                        "code '${error.code} " +
+                                        "message '${error.message}'" +
+                                        "responseInfo '${error.responseInfo}'"
+                            )
                         }
 
                         override fun onAdImpression() {
