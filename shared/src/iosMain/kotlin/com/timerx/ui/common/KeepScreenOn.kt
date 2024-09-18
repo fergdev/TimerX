c01f732f7a6b639b8ba573653b8753314e050245
fergdev <fergus.a.hewson@outlook.com>
2024-09-18 21:28:59 +1200

    Adding keep screen on for ios.

--- shared/src/iosMain/kotlin/com/timerx/ui/common/KeepScreenOn.kt
+++ shared/src/iosMain/kotlin/com/timerx/ui/common/KeepScreenOn.kt
@@ -1,8 +1,16 @@
 package com.timerx.ui.common
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import platform.UIKit.UIApplication
 
 @Composable
 actual fun KeepScreenOn() {
     // Do nothing
+    DisposableEffect(Unit){
+        UIApplication.sharedApplication.setIdleTimerDisabled(true)
+        onDispose {
+            UIApplication.sharedApplication.setIdleTimerDisabled(false)
+        }
+    }
 }
