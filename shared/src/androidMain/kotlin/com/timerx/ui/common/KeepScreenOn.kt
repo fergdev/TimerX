149e4b6077a8bd64287e9379459ebf631d2e3423
fergdev <fergus.a.hewson@outlook.com>
2024-11-01 05:20:53 +1300

    Fix for lint.

--- shared/src/androidMain/kotlin/com/timerx/ui/common/KeepScreenOn.kt
+++ shared/src/androidMain/kotlin/com/timerx/ui/common/KeepScreenOn.kt
@@ -1,19 +1,17 @@
 package com.timerx.ui.common
 
-import android.app.Activity
-import android.view.WindowManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 
 @Composable
 actual fun KeepScreenOn() {
-    val activity = LocalContext.current as Activity
+    val localView = LocalView.current
 
     DisposableEffect(Unit) {
-        activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        localView.keepScreenOn = true
         onDispose {
-            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            localView.keepScreenOn = false
         }
     }
 }
