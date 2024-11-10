5d83cd6de5be47fb6a3506577746c6fc3cd4dd02
fergdev <fergus.a.hewson@outlook.com>
2024-11-11 09:48:22 +1300

    Fix for malformed change log. Added tests for changelog.

---
+++ shared/src/commonTest/kotlin/com/timerx/resources/ChangeLogTest.kt
@@ -0,0 +1,21 @@
+package com.timerx.resources
+
+import com.timerx.testutil.NotAndroidCondition
+import com.timerx.ui.settings.about.changelog.ChangeLogItem
+import com.timerx.ui.settings.about.changelog.FILES_CHANGE_LOG_JSON
+import io.kotest.core.annotation.EnabledIf
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import kotlinx.serialization.json.Json
+import timerx.shared.generated.resources.Res
+
+
+@Suppress("RUNTIME_ANNOTATION_NOT_SUPPORTED")
+@EnabledIf(NotAndroidCondition::class)
+class ChangeLogTest : FreeSpec({
+    "current change log can be parsed" {
+        val logs = Res.readBytes(FILES_CHANGE_LOG_JSON).decodeToString()
+        val items = Json.decodeFromString<List<ChangeLogItem>>(logs)
+        items.size shouldBeGreaterThan 3
+    }
+})
